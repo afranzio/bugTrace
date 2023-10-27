a4746d4f696c5514e307f8a215e16f8088d03cb0
afranzio <maniafranzio1234@gmail.com>
2023-10-27 18:38:40 +0530

    Issues CRUD API

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -7,6 +7,12 @@ const createIssueSchema = z.object({
     description: z.string().min(1)
 })
 
+const getIDFromParams = (request: NextRequest) => {
+    const {searchParams} = new URL(request.url);
+    const param = Number(searchParams.get("id"));
+    return param;
+}
+
 export async function POST (request: NextRequest){
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
@@ -20,4 +26,62 @@ export async function POST (request: NextRequest){
 
     return NextResponse.json(newIssue, { status: 201})
 
+}
+
+export async function PUT (request: NextRequest){
+    const param = getIDFromParams(request)
+    const body = await request.json();
+    const validation = createIssueSchema.safeParse(body);
+    if (!validation.success){
+        return NextResponse.json(validation.error.errors, { status: 400 })
+    }
+
+    try {
+        const deleteIssue = await prisma.issue.update({
+            where: {
+                id: param
+            },
+            data: {title: body.title, description: body.description} 
+        })
+        return NextResponse.json(deleteIssue, { status: 202})
+    } catch (error) {
+        return NextResponse.json(error , { status: 401})
+    }
+}
+
+export async function DELETE (request: NextRequest){
+    const param = getIDFromParams(request)
+
+    if(param){
+        try {
+            const deleteIssue = await prisma.issue.delete({
+                where: {
+                    id: param
+                }
+            })
+            return NextResponse.json(deleteIssue, { status: 202})
+        } catch (error) {
+            return NextResponse.json(error , { status: 401})
+        }
+    }
+}
+
+export async function GET (request: NextRequest){
+    const param = getIDFromParams(request)
+
+    if(param){
+        try {
+            const getIssue = await prisma.issue.findUniqueOrThrow({
+                where: {
+                    id: param
+                }
+            })
+            return NextResponse.json(getIssue, { status: 200})
+        } catch (error) {
+            return NextResponse.json(error , { status: 401})
+        }
+    }
+
+    const getIssues = await prisma.issue.findMany()
+    return NextResponse.json(getIssues, { status: 200})
 }
