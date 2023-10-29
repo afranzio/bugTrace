4c795c53f5db148659d201693e9d69e93573c353
afranzio <maniafranzio1234@gmail.com>
2023-10-29 09:42:55 +0530

    Samples

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -9,6 +9,7 @@ const CreateIssue = () => {
     const goBack = () => {
         router.back();
     };
+
     return (
         <div className='container w-full md:w-1/2'>
             <form>
@@ -26,6 +27,8 @@ const CreateIssue = () => {
                                         name="title"
                                         id="title"
                                         autoComplete="given-name"
+                                        minLength={1}
+                                        max={255}
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     />
                                 </div>
@@ -40,6 +43,7 @@ const CreateIssue = () => {
                                         id="description"
                                         name="description"
                                         rows={3}
+                                        minLength={1}
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         defaultValue={''}
                                     />
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -9,8 +9,14 @@ interface Issue {
 
 const Issues = async () => {
 
-  const response = await fetch('http://localhost:3000/api/issues', { cache: 'no-store' })
-  const data: Issue[] = await response.json();
+  let data: Issue[] = []
+  try {
+    const response = await fetch('http://localhost:3000/api/issues', { cache: 'no-store' })
+    data = await response.json();
+  } catch (error) {
+    console.log("API error")
+  }
+
 
   const dateConversion = (date: string) => {
     var dateTime = new Date(date);
@@ -20,22 +26,31 @@ const Issues = async () => {
   return (
     <div>
       <main className="container w-full md:w-1/2">
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Created At</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map(user => <tr key={user.id}>
-              <td className="capitalize" >{user.title}</td>
-              <td>{dateConversion(user.createdAt)}</td>
-              <td>{user.description}</td>
-            </tr>)}
-          </tbody>
-        </table>
+        <h2 className="text-xl border-b border-gray-900/10 pb-3 font-semibold leading-7 text-gray-900 subpixel-antialiased">Issues</h2>
+        {
+          data.length > 0 ?
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Created At</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map(user => <tr key={user.id}>
+                  <td className="capitalize" >{user.title}</td>
+                  <td>{dateConversion(user.createdAt)}</td>
+                  <td>{user.description}</td>
+                </tr>)}
+              </tbody>
+            </table>
+            :
+            <div className="alert alert-error mt-5">
+              <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              <span>Error! API failed. Please reach out Admin</span>
+            </div>
+        }
       </main>
     </div>
   )
