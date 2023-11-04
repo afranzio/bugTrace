6320fb1a8eb3db688c626e7706b3d2a7022fb005
afranzio <maniafranzio1234@gmail.com>
2023-11-04 19:36:35 +0530

    seclection

--- app/components/dropdown/dropdown.tsx
+++ app/components/dropdown/dropdown.tsx
@@ -1,9 +1,17 @@
 'use client';
 
-import React from 'react'
+import React, { useState } from 'react';
 
 
 const DropDownMenu = ({ userStatus }: any) => {
+    const [selectedOption, setSelectedOption] = useState('OPEN');
+
+    const handleIssueStatusChange = async (event: object) => {
+        const selectedValue = event.target.value;
+        console.log(selectedValue);
+        setSelectedOption(selectedValue);
+    }
+
     const status = {
         "OPEN": "Open",
         "CLOSE": "Close",
@@ -13,8 +21,8 @@ const DropDownMenu = ({ userStatus }: any) => {
 
     return (
         <div>
-        <select className="border-none text-sm py-0 focus-visible:border-none">
-                {Object.entries(status).map(([key, value]) => <option className="d-flex justify-between text-sm px-5 py-0" key={key} selected={key==userStatus.status ? true : false } >{value}</option>)}
+            <select className="border-none text-sm py-0 focus-visible:border-none" value={selectedOption} onChange={handleIssueStatusChange}>
+                {Object.entries(status).map(([key, value]) => <option className="d-flex justify-between text-sm px-5 py-0" key={key} id={userStatus.id} selected={key==userStatus.status ? true : false } >{value}</option>)}
             </select>
         </div>
     )
