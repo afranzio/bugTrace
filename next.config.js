a1668a2b5bceedae15ad5e818e26e2df174bbd93
afranzio <maniafranzio1234@gmail.com>
2023-11-13 11:57:24 +0530

    re

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  target: 'experimental-serverless-trace',
+  exportPathMap: async function () {
+    // your exportPathMap logic here
+    return {
+      '/': { page: '/' },
+      '/issues/board/[id]': { page: '/issues/board/[id]' },
+      // other pages...
+    };
+  },
   experimental: {
     serverActions: true,
   },
