10840e77563cd266eaf1a951fc1123544b9b4d7a
afranzio <maniafranzio1234@gmail.com>
2023-11-13 13:07:36 +0530

    Authentication - Issue was .env file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  target: 'experimental-serverless-trace',
-  exportPathMap: async function () {
-    // your exportPathMap logic here
-    return {
-      '/': { page: '/' },
-      '/issues/board/[id]': { page: '/issues/board/[id]' },
-      // other pages...
-    };
-  },
   experimental: {
     serverActions: true,
   },
